c70102b5422827b9ed3a4cce1e9f624a913383a6
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-01-17 14:49:21 +0000

    chore(release): v5.0.0

--- time/offset/offset_Offset.go
+++ time/offset/offset_Offset.go
@@ -2,10 +2,10 @@ package offset
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-time-go/time/v4/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-time-go/time/v5/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-time-go/time/v4/offset/internal"
+	"github.com/cdktf/cdktf-provider-time-go/time/v5/offset/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
--- time/staticresource/staticresource_StaticResource.go
+++ time/staticresource/staticresource_StaticResource.go
@@ -2,10 +2,10 @@ package staticresource
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-time-go/time/v4/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-time-go/time/v5/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-time-go/time/v4/staticresource/internal"
+	"github.com/cdktf/cdktf-provider-time-go/time/v5/staticresource/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
