142a9ab483c3ca76baf8cff2b7713326fb0b7736
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-08-29 15:02:47 +0000

    chore(release): v8.0.0

--- time/staticresource/StaticResource.go
+++ time/staticresource/StaticResource.go
@@ -1,11 +1,14 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package staticresource
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-time-go/time/v7/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-time-go/time/v8/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-time-go/time/v7/staticresource/internal"
+	"github.com/cdktf/cdktf-provider-time-go/time/v8/staticresource/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
